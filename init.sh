7d1defe75e5a7cb650cccb63ca6d76e4c24bbe06
JoungKyun Kim <joungkyun@gmail.com>
2016-06-18 06:11:48 +0900

    add .gitignore

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 [ -f Makefile ] && make distclean
 rm -rf modules/ build/ include/ auto*
+rm -f tests/*.diff tests/*.exp tests/*.log tests/*.out tests/*.php tests/*.sh
 mv config.m4 tests/config.m4
 rm -f .deps Makefile* ac* config.* configure* install-sh ltmain.sh missing mkinstalldirs run-tests.php
 mv tests/config.m4 ./config.m4
