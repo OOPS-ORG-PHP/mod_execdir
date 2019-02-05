f321b45a088639bdd5769a48072610c91d696d39
JoungKyun Kim <joungkyun@gmail.com>
2019-02-06 05:49:45 +0900

    update package information

--- init.sh
+++ init.sh
@@ -1,3 +1,30 @@
+
+if [ "$1" = "package" ]; then
+	list="README.md README.ko.md config.m4 execdirapi.c execdirapi.h php_execdir.c php_execdir.h proc_open.c proc_open5.c proc_open53.c"
+
+	for i in ${list}
+	do
+		md5v="$(md5sum ${i} | awk '{print $1}')"
+		perl -pi -e \
+			"s/(<file md5sum=)\"[^\"]*\" (name=\"${i}\" role=)/\1\"${md5v}\" \2/g" \
+			package.xml
+	done
+
+	for i in $(ls tests/*.*)
+	do
+		fname=$(basename $i)
+		md5v="$(md5sum ${i} | awk '{print $1}')"
+		perl -pi -e \
+			"s/(<file md5sum=)\"[^\"]*\" (name=\"${fname}\" role=)/\1\"${md5v}\" \2/g" \
+			package.xml
+	done
+
+	pecl package
+	exit 0
+fi
+
+
+
 [ -f Makefile ] && make distclean
 rm -rf modules/ build/ include/ auto*
 rm -f tests/*.diff tests/*.exp tests/*.log tests/*.out tests/*.php tests/*.sh
