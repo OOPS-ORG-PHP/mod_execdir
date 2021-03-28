e46ec62bb7562efdaf1898fa2f68ccd47e03eddb
JoungKyun Kim <joungkyun@gmail.com>
2021-03-29 04:17:08 +0900

    enhanced build test environments for legacy PHP (5.0 to 5.5}

--- manage.sh
+++ manage.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 source /usr/share/annyung-release/functions.d/bash/functions
+setAnsi
+tcolor="${bblack}${bgwhite}"
 
 errmsg () {
 	echo "$*" 1>&2
 }
 
 usage () {
-	echo "Usage: $0 [clean|pack|test [php-version]]"
+	echo "${bwhite}Usage:${normal} $0 [clean|pack|test [php-version]]"
 	exit 1
 }
+
+mod_name="$( grep "^ZEND_GET_MODULE" *.c | grep -Po '(?<=\()[a-z]+(?=\))' )"
 
 opts=$(getopt -u -o h -l help -- "$@")
 [ $? != 0 ] && usage
@@ -34,15 +38,16 @@ mode="${1}"
 case "${mode}" in
 	clean)
 		cat <<-EOL
-			[ -f Makefile ] && make distclean
+			${bwhite}[ -f Makefile ] && make distclean
 			rm -rf autom4te.cache build include modules
-			rm -f .deps Makefile* ac*.m4 compile
+			rm -f .deps Makefile* ac*.m4 dyn*.m4 compile
 			rm -f config.h* config.nice configure* config.sub config.guess
 			rm -f install-sh ltmain.sh missing mkinstalldirs run-tests.php
 
 			rm -f package.xml
-			find ./tests ! -name '*.phpt' -a ! -name '*.txt' -a -type f
-			----->
+			rm -f tests/*.{diff,exp,log,out,php,sh,mem}
+			rm -f ${mod_name}_arginfo.h && git checkout -- ${mod_name}_arginfo.h
+			${normal}----->
 		EOL
 
 		[ -f Makefile ] && make distclean
@@ -52,7 +57,8 @@ case "${mode}" in
 		rm -f install-sh ltmain.sh missing mkinstalldirs run-tests.php
 
 		rm -f package.xml
-		find ./tests ! -name '*.phpt' -a ! -name '*.txt' -a -type f
+		rm -f tests/*.{diff,exp,log,out,php,sh,mem}
+		rm -f ${mod_name}_arginfo.h && git checkout -- ${mod_name}_arginfo.h
 		;;
 	pack)
 		cp -af package.xml.tmpl package.xml
@@ -74,21 +80,72 @@ case "${mode}" in
 		rm -f package.xml
 		;;
 	test)
+		# support PHP 4.3 and after
+
+		PHPBIN=/opt/php-qa/php${2}/bin/php
+		PHPIZE=/opt/php-qa/php${2}/bin/phpize
+		PHPCONFIG=/opt/php-qa/php${2}/bin/php-config
+		PHP_OPT=""
+		LEGACY_TEST=0
+
+		if [[ $# == 2 ]]; then
+			./manage.sh clean
+			echo "${PHPIZE} && ./configure"
+			${PHPIZE} && ./configure && make -j8 || exit 0
+
+			if [[ ! -f ./run-tests.php ]]; then
+				LEGACY_TEST=1
+				PHP_OPT="-n -d 'enable_dl=1' -d 'safe_mode=0' -d 'disable_error=0'"
+			fi
+		fi
+		PHP_OPT+=" -d 'track_errors=1' -d 'extension_dir=./modules/' -d 'extension=${mod_name}.so'"
+
 		if [[ -f tests/${3}.php ]]; then
-			/usr/bin/php${2} -d "extension_dir=./modules/" -d "extension=korean.so" tests/${3}.php
+			cat <<-EOL
+				${bgreen}------------------------------------------------------------------------
+				Sample code execution:
+
+				${bcyan}${PHPBIN} ${PHP_OPT} test/${3}.php
+				${bgreen}------------------------------------------------------------------------${normal}
+
+			EOL
+
+			${PHPBIN} ${PHP_OPT} test/${3}.php
 			exit $?
 		elif [[ -f ${3} ]]; then
-			/usr/bin/php${2} -d "extension_dir=./modules/" -d "extension=korean.so" ${3}
+			cat <<-EOL
+				${bgreen}------------------------------------------------------------------------
+				Sample code execution:
+
+				${bcyan}${PHPBIN} ${PHP_OPT} ${3}
+				${bgreen}------------------------------------------------------------------------${normal}
+
+			EOL
+
+			eval "${PHPBIN} ${PHP_OPT} ${3}"
 			exit $?
 		fi
 
-		if [[ -z $3 ]]; then
-			./manage.sh clean
-			echo "phpize${2} ./configure"
-			phpize${2} && ./configure && make -j8 || exit 0
-		fi
-		echo "make test PHP_EXECUTABLE=/usr/bin/php${2}"
-		make test PHP_EXECUTABLE=/usr/bin/php${2} <<< n
+		grep "^PHP_DEPRECATED_DIRECTIVES_REGEX =" Makefile | grep -q "safe_mode"
+		[[ $? == 0 ]] && {
+			perl -pi -e 's/safe_mode\|//g' Makefile
+		}
+
+		cat <<-EOL
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			${tcolor}** MAKE test::                                                       ${normal}
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			${bcyan}make test PHP_EXECUTABLE=${PHPBIN}
+			${bwhite}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${normal}
+
+		EOL
+
+		(( LEGACY_TEST == 1 ))                                                   \
+			&& PHP_EXECUTABLE=${PHPBIN} bash /opt/php-qa/TEST-ENV/legacy-test.sh \
+			|| make test PHP_EXECUTABLE=${PHPBIN} <<< n
+
+		exit $?
 		;;
 	*)
 		errmsg "Unsupport mode '${1}'"
